c8d947decbc0f8a171eed4f57ca506eacb950095
Aanisha Mishra <aanisha.mishra05@gmail.com>
2019-01-29 12:15:00 +0530

    Create team_meeting.py

---
+++ examples/team_meeting.py
@@ -0,0 +1,15 @@
+'''
+Schedule a team meeting on first Friday of every month
+'''
+import calendar
+
+print("Team meeting will be on: ")
+for m in range(1,13):
+    cal = calendar.monthcalendar(2018,m)
+    weekone = cal[0]
+    weektwo = cal[1]
+    if weekone[calendar.FRIDAY] !=0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+    print("%10s %2d" %(calendar.month_name[m], meetday))
