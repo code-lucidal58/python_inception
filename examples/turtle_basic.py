765e8ea8fd356ca84a13eb848e3cebf2678a7ff6
code-lucidal58 <aanisha.mishra05@gmail.com>
2020-05-22 18:12:01 +0530

    added turtle

---
+++ examples/turtle_basic.py
@@ -0,0 +1,15 @@
+import turtle
+##sudo apt-get install python-tk
+## run this above command in ubuntu terminal if you not able to run this code
+t=turtle.Turtle()
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+turtle.done()
+## THis command holds the python turtle graphics window till you close it yourself
