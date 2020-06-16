5ec4b192307247e9082aa11607f11a5158065b2d
code-lucidal58 <aanisha.mishra05@gmail.com>
2020-06-16 12:43:45 +0530

    code formatting

--- examples/april_fool_day.py
+++ examples/april_fool_day.py
@@ -1,14 +1,11 @@
 # This program is to calculate the next April fool's day
-from datetime import date 
-from datetime import time
-from datetime import datetime
-from datetime import timedelta
+from datetime import date
 
 today = date.today()
-afd = date(today.year, 4, 1) # Feed AFD
+afd = date(today.year, 4, 1)  # Feed AFD
 
 if afd < today:
-    print("April Fool's day already went by %d days ago" % (today-afd).days)
-    afd = afd.replace(year = today.year+1)
-time_to_afd = afd - today # this create time.delta
+    print("April Fool's day already went by %d days ago" % (today - afd).days)
+    afd = afd.replace(year=today.year + 1)
+time_to_afd = afd - today  # this create time.delta
 print("It's just", time_to_afd.days, "days until April fool's day")
